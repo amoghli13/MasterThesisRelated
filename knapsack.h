23fe8df591b32496aef86b8446181e39d96a884c
moghliam <moghliam@gmail.com>
2013-07-14 23:25:31 -0500

    Sync_1

--- knapsack.h
+++ knapsack.h
@@ -31,7 +31,7 @@ public:
 		for(int i=0;i<num_items;i++)
 		{
 			weights[i]=rand()%5;
-			weights[i]=weights[i]+3;
+			weights[i]=weights[i]+1;
 			values[i]=rand()%100;
 			eval_mat[i]=new error_inject_operators<int>[max_weight];
 		}
