b7b0562bf84f3d670afb761063b840db9ff50e67
moghliam <moghliam@gmail.com>
2013-07-21 23:57:57 -0500

    Some pseudo changes-1

--- djikstra.cpp
+++ djikstra.cpp
@@ -47,29 +47,35 @@ void sortMyNode(int v)
     }  */  
 }
  
-void trace_fault(int v,vector<int>& accepted_nodes,vector<int>& accepted_nodes_ptr,int accepted_nodes_idx)
-{
-int faulty_node=G[v][accepted_nodes_ptr[accepted_nodes_idx]-1].first; 
-int fault_node_idx;
-int sz=accepted_nodes.size();
-for( int i=0;i<sz;i++ )
-{
-	if( accepted_nodes[i]==faulty_node )
-	{
-		fault_node_idx=i;
-		i+=sz;
-	}
-}
-int start_ptr=accepted_nodes[fault_node_idx];
-if( start_ptr )
-{
-
-
-
-}
-
-return;
-} 
+ inline error_inject_operators<int> tmr_lesser( error_inject_operators<int> a1,int a2)
+ {
+ 	error_inject_operators<int> res1,res2,res3;
+ 	res1= a1 < a2;
+ 	res2= a1 < a2;
+ 	res3= a1 < a2;
+ 	error_inject_operators<int> result;
+ 	if(  res1 == res2 )
+ 	{
+ 		result=res1;
+ 	}
+ 	else if (res2 == res3) 
+ 	{
+ 		result=res2; 	
+ 	}
+ 	else if (res1 == res3)
+ 	{
+ 		result=res3; 	
+ 	}
+ 	else
+ 	{
+	 	cout<<"\n\t res1: "<<res1<<" res2: "<<res2<<" res3: "<<res3<<" are not equal and hence, exitting! \n ";
+	 	exit(-1);
+ 	}
+ 	
+ 	cout<<"\n\t res1: "<<res1<<" res2: "<<res2<<" res3: "<<res3<<" result "<<result;
+ 	return result;
+ }
+ 
  
 void Dijkstra(int s,int destination)
 {
@@ -104,27 +110,36 @@ void Dijkstra(int s,int destination)
 	    	if( G[accepted_nodes[i]][accepted_nodes_ptr[i]].second < G[reqd_idx_in_accepted_nodes][accepted_nodes_ptr[reqd_idx_in_accepted_nodes]].second )
 	    	{
 	    		reqd_idx_in_accepted_nodes=i;
-	    		//cout<<"\n\t"<<G[accepted_nodes[i]][accepted_nodes_ptr[i]].second << " is lesser than "<<G[accepted_nodes[i-1]][accepted_nodes_ptr[i-1]].second ;
+	    		cout<<"\n\t"<<G[accepted_nodes[i]][accepted_nodes_ptr[i]].second << " is lesser than "<<G[accepted_nodes[i-1]][accepted_nodes_ptr[i-1]].second ;
+    	    		cout<<"\n\t v: "<<accepted_nodes[i]<<"\t v2: "<<G[accepted_nodes[i]][accepted_nodes_ptr[i]].first;
 	    		// exit(-1);
 	    	}
 	    }
 
+    	    		cout<<"\n\t v: "<<accepted_nodes[reqd_idx_in_accepted_nodes]<<"\t v2: "<<G[accepted_nodes[reqd_idx_in_accepted_nodes]][accepted_nodes_ptr[reqd_idx_in_accepted_nodes]].first;
+	    	int v=accepted_nodes[reqd_idx_in_accepted_nodes];
 		ii tmp_reqd_ptr=G[v][accepted_nodes_ptr[reqd_idx_in_accepted_nodes]];
 		
 		if( accepted_nodes_ptr[reqd_idx_in_accepted_nodes] )
 		{
-			if( tmp_reqd_ptr.second < G[v][accepted_nodes_ptr[reqd_idx_in_accepted_nodes]-1].second )
+			error_inject_operators<int> duh;
+			duh=G[v][accepted_nodes_ptr[reqd_idx_in_accepted_nodes]-1].second;
+			if( tmp_reqd_ptr.second < duh )
 			{
-				trace_fault( v, accepted_nodes,accepted_nodes_ptr,reqd_idx_in_accepted_nodes );
+				//trace_fault( v, accepted_nodes,accepted_nodes_ptr,reqd_idx_in_accepted_nodes );
+				cout<<"\n\t FATAL edge with weight "<<tmp_reqd_ptr.second<<" was decided to be smaller than "<<duh;
 			} 
 		}
-	    	int v=accepted_nodes[reqd_idx_in_accepted_nodes];
+
 	    	int v2=tmp_reqd_ptr.first;
 	    	error_inject_operators<int> cost;
 	    	cost=tmp_reqd_ptr.second;
 	    	cout<<"\n\t Considering accepted node: "<<v<<" and it's current viable node is: "<<v2<<" and the edge b/n them has a weight of "<<cost;
-	    	int cost_compare=cost+D[v];
-	    	if( D[v2] > cost_compare )
+	    	error_inject_operators<int> cost_compare;
+	    	cost_compare=cost+D[v];
+	   
+	    	//if( (D[v2] > cost_compare ) 
+	    	if( tmr_lesser( cost_compare,D[v2])==1 )
 	    	{
 	    	
 		    	if(accepted_nodes_hash[v2]!=1)
@@ -139,16 +154,18 @@ void Dijkstra(int s,int destination)
   			}
   			//else
   			accepted_nodes_ptr[reqd_idx_in_accepted_nodes]+=1;
-		    	D[v2]=cost_compare;
+		    	D[v2]=cost_compare.operand;
 			//cout<<"\n Illi Saddu maadu accepted_nodes_ptr[reqd_idx_in_accepted_nodes] "<<accepted_nodes_ptr[reqd_idx_in_accepted_nodes]<<" and reqd_idx_in_accepted_nodes is "<<reqd_idx_in_accepted_nodes;
 		}
-		else if ( accepted_nodes_hash[v2]==1 )
+		else if (D[v2]==0)//(accepted_nodes_hash[v2]==1 )
 		{
+			cout<<"\n\t Viable node: "<<v2<<" is accepted! since D[v2]: "<< D[v2] <<" is NOT greater than "<<cost_compare; //and the edge b/n them has a weight of "<<cost;
 			accepted_nodes_ptr[reqd_idx_in_accepted_nodes]+=1;
+			//exit(-1);
 			//cout<<"\n\t Alli saddu maadu! accepted_nodes_ptr[reqd_idx_in_accepted_nodes] "<<accepted_nodes_ptr[reqd_idx_in_accepted_nodes]<<" and reqd_idx_in_accepted_nodes is "<<reqd_idx_in_accepted_nodes;
 		}
-		//else
-			//cout<<"\n\t Alli illa illu illa saddu maadu! \n";		
+		else
+			cout<<"\n\t Viable node: "<<v2<<" D[v2]: "<< D[v2] <<" "<<cost_compare; //and the edge b/n them has a weight of "<<cost;
 		
 		
     	if( v2==destination) 
@@ -166,19 +183,28 @@ int main()
 
     int m, s, t = 0;
     printf("\n\t Enter: \n\t\t 1. number of nodes \n\t\t 2. Number of edges \n\t\t 3. Source node \n\t\t 4. Destination node \n\t\t ");
-    scanf("%d %d %d %d", &n, &m, &s, &t);
+    //scanf("%d %d %d %d", &n, &m, &s, &t);
+		
+   FILE* ip_file_handle;
+   string ip_file="inputs.txt";
+   ip_file_handle=fopen(ip_file.c_str(),"r");
 
-	 error_inject_operators<int>::error_percent=0.01; 
+    fscanf(ip_file_handle," %d %d %d %d",&n,&m,&s,&t);
+    printf("\n\t N: %d M: %d S: %d T: %d ",n,m,s,t);
+
+	 error_inject_operators<int>::error_percent=0.0; 
     for (int i = 0; i < m; i++)
     {
         int a, b, w = 0;
-        scanf("%d %d %d", &a, &b, &w);
+        //scanf("%d %d %d", &a, &b, &w);
+        fscanf( ip_file_handle,"%d %d %d", &a, &b, &w);
+        printf("\n %d %d %d\n", a, b, w);
         error_inject_operators<int> tmp;
         tmp=w;
         G[a - 1].push_back(ii(b - 1, tmp));
         G[b - 1].push_back(ii(a - 1, tmp));
     }
- 
+    //cout<<endl;exit(-1);
     Dijkstra(s - 1,t-1);
  
     printf("\n\n\t Result: %d\n", D[t - 1]);
