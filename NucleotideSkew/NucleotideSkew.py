c2021dfc03cac5e5eac1b4e41c4170cd5f1faf04
amoghavs <amoghavs@gmail.com>
2014-05-19 01:32:47 -0700

    NucleotideSkew: Gene range merging of protein and rna genes

--- NucleotideSkew/NucleotideSkew.py
+++ NucleotideSkew/NucleotideSkew.py
@@ -1,8 +1,141 @@
 
-# /usr/bin/python
+#! /usr/bin/python
 
-use sys,re
+import sys,getopt,re
+
+def usage():
+	print "\n\t NucleotideSkew.py -g <GENOME-ID> \n\t Optional: -h<help>"
+	FileCheck()
+	sys.exit()
+	
+def FileCheck():
+	print "\n\t Following files are expected: *.fna *.ptt *.rnt in this folder \n"
+def WhiteSpace(arg):
+	tmp=re.sub('^\s*','',arg)
+	tmp1=re.sub('\s*$','',tmp)
+	#print "\n\t Arg "+str(arg)+" tmp "+str(tmp)+" tmp1 "+str(tmp1)
+	return tmp1
+
+def FindGeneRanges(GeneFile,GeneType):
+	NumLinesGenes=len(GeneFile)
+	print "\n\t Number of lines in gene report file is "+str(NumLinesGenes)+" the genes are supposed to code "+str(GeneType)
+	HeaderLineNum=0
+
+	for CurrLine in GeneFile:
+		HeaderLineNum+=1
+		HeaderLine=re.match('\s*Location\s*Strand',CurrLine)
+		#print "\n\t Checking line: "+str(HeaderLineNum)
+		if HeaderLine:
+			print "\n\t Header for protein gene found at line "+str(HeaderLineNum)+"\n\t Header: "+str(CurrLine)
+			break
+		
+	
+	GeneRanges=[]
+	NumGene=0
+	for LineNum in range(HeaderLineNum,NumLinesGenes):
+		CurrLine=re.match('(\d+)*\.\.(\d+)*',GeneFile[LineNum])
+		if CurrLine:
+			Range=[]
+			#print "\n\t LineNum: "+str(LineNum)+" CurrLine "+str(CurrLine.group(0))
+			Range.append(CurrLine.group(1))
+			Range.append(CurrLine.group(2))
+			GeneRanges.append(Range)
+			NumGene+=1
+		else:
+			print "\n\t Didnt find nucleotide range in lineNum: "+str(LineNum)+" CurrLine "+str(GeneFile[LineNum])
+			
+		
+	print "\n\t Found "+str(NumGene)+" genes. \n"
+	return GeneRanges
+	
+
+def main(argv):
+	GenomeID=''
+	debug=0
+	try:
+		opts, args = getopt.getopt(argv,"hg:v:",["genomeID=","verbose="])
+	except getopt.GetoptError:
+		print "\n\t ##$$ \n"
+		usage()
+		sys.exit()	
+	for opt, arg in opts:
+		if opt == '-h':
+			usage()
+		elif opt in ("-g", "--genomeID"):
+			tmp=WhiteSpace(arg)
+			GenomeID=str(tmp)
+			print "\n\t GenomeID option is "+str(GenomeID);
+		else:
+			usage()
+	
+	FileCheck()
+	ProteinGeneFileName=GenomeID+str('.ptt')
+	File1=open(ProteinGeneFileName)
+	ProteinGene=File1.readlines()
+	File1.close()
+	
+	ProteinGeneRanges=FindGeneRanges(ProteinGene,"Protein") # Assumed that each member will have an array with 1st and 2nd member indicating start and end nucleotide respectively.
+	
+	ProteinGeneNum=len(ProteinGeneRanges)
+	
+	RnaGeneFileName=GenomeID+str('.rnt')
+	File1=open(RnaGeneFileName)
+	RnaGene=File1.readlines()
+	File1.close()
+	
+	RnaGeneRanges=FindGeneRanges(RnaGene,"Rna")	
+	RnaGeneNum=len(RnaGeneRanges)
+
+	TotalMaxGeneNum=0
+	TotalMinGeneNum=0
+	if(ProteinGeneNum > RnaGeneNum):
+		MaxGene=ProteinGeneRanges
+		MinGene=RnaGeneRanges
+		TotalMaxGeneNum=ProteinGeneNum
+		TotalMinGeneNum=RnaGeneNum
+	else:
+		MaxGene=ProteinGeneRanges
+		MinGene=RnaGeneRanges	
+		TotalMaxGeneNum=RnaGeneNum
+		TotalMinGeneNum=ProteinGeneNum		
+
+	TotalGeneRanges=[]
+	TotalGeneNum=RnaGeneNum+ProteinGeneNum
+	MinGeneNum=0
+	MaxGeneNum=0	
+		
+	print "\n\t TotalGeneNum "+str(TotalGeneNum)+" ProteinGeneNum "+str(ProteinGeneNum)+" RnaGeneNum "+str(RnaGeneNum)
+
+	for CurrRangeNum in range(TotalGeneNum):
+		# Since Gene ranges are assumed to be non-overlapping, hence it must be sufficient to just compare start ranges! 
+		#print "\n\t MaxGeneNum "+str(MaxGeneNum)+" MinGeneNum "+str(MinGeneNum)
+		if ( (MaxGene[MaxGeneNum][0] > MinGene[MinGeneNum][0]) ): #and (MaxGene[MaxGeneNum][0] > MinGene[MinRangeNum][1]) ):
+			TotalGeneRanges.append(MinGene[MinGeneNum])
+			MinGeneNum+=1
+			#print "\n\t MinGeneNum "+str(MinGeneNum)+" TotalMinGeneNum "+str(TotalMinGeneNum)
+			if( MinGeneNum >= TotalMinGeneNum):
+				for RangeNum in range(MaxGeneNum,TotalGeneNum-MinGeneNum):
+					TotalGeneRanges.append(MaxGene[RangeNum])
+				break
+		else:
+			TotalGeneRanges.append(MaxGene[MaxGeneNum])
+			MaxGeneNum+=1
+			#print "\n\t MaxGeneNum "+str(MaxGeneNum)+" TotalMaxGeneNum "+str(TotalMaxGeneNum)
+			if( MaxGeneNum >= TotalMaxGeneNum):
+				for RangeNum in range(MinGeneNum,TotalGeneNum-MaxGeneNum):
+					TotalGeneRanges.append(MinGene[RangeNum])
+				break
 
 
-def main()
+			
+	print "\n\t TotalGeneNum "+str(len(TotalGeneRanges))
 
+	#for CurrRange in TotalGeneRanges:
+	#	print "\n\t Gene-start "+str(TotalGeneRanges[CurrRange][0])+" end "+str(TotalGeneRanges[CurrRange][1])
+	
+	
+	
+	
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
