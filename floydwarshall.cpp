a057f7089c6150b90f7082baee10a1c1ddf7178e
amoghavs <amoghavs@gmail.com>
2014-06-18 01:31:48 -0700

    Working version of FW algorithm added

--- floydwarshall.cpp
+++ floydwarshall.cpp
@@ -14,18 +14,46 @@ typedef vector<vii> vvii;
 const int MAX = 1001;
 const int MAXINT = 1000000000;
 
-#define INTMAX 2147483640 
+#define INTMAX 10000 
  
 template <class M>
 double  error_inject_operators<M>::error_percent=0.0;
+int n;
 
- 
+void FloydWarshall(error_inject_operators<int>** UpdateMat,double ErrorPercent)
+{
+	error_inject_operators<int>::error_percent=ErrorPercent;
+	
+    for(int i=0;i<n;i++)
+    {
+        
+		for(int j=0;j<n;j++)
+		{
+			//cout<<"\t "<<(UpdateMat[i][j]); 
+			for(int k=0;k<j;k++)
+				if( (UpdateMat[i][j]) > (UpdateMat[i][k]+UpdateMat[k][j]) )
+				{
+					cout<<"\n\t i "<<i<<" j "<<j<<" k "<<k<<" LHS: "<<(UpdateMat[i][j])<<" is greater than RHS: "<<(UpdateMat[i][k])<<" , "<<(UpdateMat[k][j]);
+					UpdateMat[i][j]= (UpdateMat[i][k]+UpdateMat[k][j]); 
+				}				
+				
+		}
+	}
+	cout<<"\n\n";
+    for(int i=0;i<n;i++)
+    {
+        cout<<"\n";
+		for(int j=0;j<n;j++)
+			cout<<"\t "<<(UpdateMat[i][j]); 
+	}
+
+}
 
  
 int main()
 {
 
-    int n,m, s, t = 0;
+    int m, s, t = 0;
     printf("\n\t Enter: \n\t\t 1. number of nodes \n\t\t 2. Number of edges \n\t\t 3. Source node \n\t\t 4. Destination node \n\t\t ");
     //scanf("%d %d %d %d", &n, &m, &s, &t);
 		
@@ -38,15 +66,15 @@ int main()
 
 	error_inject_operators<int>::error_percent=0.0; 
 	
-	error_inject_operators<int>** Mat;
-	Mat=new error_inject_operators<int>*[n];
+	error_inject_operators<int>** UpdateMat;
+	UpdateMat=new error_inject_operators<int>*[n];
 	
 	for(int i=0; i<n;i++)
-		Mat[i]=new error_inject_operators<int> [n];
+		UpdateMat[i]=new error_inject_operators<int> [n];
 	
 	for(int i=0;i<n;i++)
 		for(int j=0;j<n;j++)
-			Mat[i][j]= INTMAX ;
+			UpdateMat[i][j]= INTMAX ;
 	
 	
     for (int i = 0; i < m; i++)
@@ -57,9 +85,9 @@ int main()
         printf("\n %d %d %d %d\n", a, b, w,d);
  
 		if(d)		      
-			Mat[a][b]=w;
+			UpdateMat[a][b]=w;
 		else
-			Mat[b][a]=w;
+			UpdateMat[b][a]=w;
       
     }
     
@@ -67,9 +95,10 @@ int main()
     {
         cout<<"\n";
 		for(int j=0;j<n;j++)
-			cout<<"\t "<<(Mat[i][j]); //Mat[i][j]= INTMAX ;
+			cout<<"\t "<<(UpdateMat[i][j]); 
 	}
-    
+    cout<<"\n\n";
+    FloydWarshall(UpdateMat,0.0);
     cout<<"\n\n";exit(-1);
  
     return 0;
